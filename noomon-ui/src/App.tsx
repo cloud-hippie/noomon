3bb4b0e6e87ca263c0ec8fc094272f9fd7766c0e
Justin Rice <jsrice7391@gmail.com>
2023-06-04 13:08:18 -0400

    Verified the proxy works

--- noomon-ui/src/App.tsx
+++ noomon-ui/src/App.tsx
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [message, setMessage] = useState<string>("");
+  useEffect(() => {
+    fetch("/hello")
+      .then((res) => res.json())
+      .then((data) => {
+        setMessage(JSON.stringify(data));
+      });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +20,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <p>{message}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
